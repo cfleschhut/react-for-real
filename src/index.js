11f03fa775c855edee77abbce273988e5947987f
Christian Fleschhut <weltrekordhalter@gmail.com>
2020-01-02 16:32:40 +0100

    Chapter 5 (Redux actions, reducers, store, devtools)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import WordCounter from './components/WordCounter/WordCounter';
 
+import store from './components/MovieGuide/store';
+import { requestMovies } from './components/MovieGuide/movieApi';
+import { MOVIES_LOADED } from './components/MovieGuide/actions';
+
+requestMovies().then(movies => {
+  store.dispatch({ type: MOVIES_LOADED, movies });
+});
+
 ReactDOM.render(
   <WordCounter targetWordCount={10} />,
   document.getElementById('app'),
